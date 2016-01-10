ad2a645f69faa4661c4f736f809dfac3e890d600
Dave Murphy <davem@devkitpro.org>
2016-01-10 23:21:09 +0000

    tidy up formatting

--- audio/streaming/source/main.c
+++ audio/streaming/source/main.c
@@ -9,117 +9,119 @@
 #define SAMPLESPERBUF (SAMPLERATE / 30)
 #define BYTESPERSAMPLE 4
 
+//----------------------------------------------------------------------------
 void fill_buffer(void *audioBuffer,size_t offset, size_t size, int frequency ) {
+//----------------------------------------------------------------------------
 
-  u32 *dest = (u32*)audioBuffer;
+	u32 *dest = (u32*)audioBuffer;
 
-  for (int i=0; i<size; i++) {
+	for (int i=0; i<size; i++) {
 
-    s16 sample = INT16_MAX * sin(frequency*(2*M_PI)*(offset+i)/SAMPLERATE);
+		s16 sample = INT16_MAX * sin(frequency*(2*M_PI)*(offset+i)/SAMPLERATE);
 
-    dest[i] = (sample<<16) | (sample & 0xffff);
-  }
-  DSP_FlushDataCache(audioBuffer,size);
+		dest[i] = (sample<<16) | (sample & 0xffff);
+	}
+
+	DSP_FlushDataCache(audioBuffer,size);
 
 }
 
+//----------------------------------------------------------------------------
 int main(int argc, char **argv) {
+//----------------------------------------------------------------------------
 
-  PrintConsole topScreen;
-  ndspWaveBuf waveBuf[2];
+	PrintConsole topScreen;
+	ndspWaveBuf waveBuf[2];
 
-  gfxInitDefault();
+	gfxInitDefault();
 
-  consoleInit(GFX_TOP, &topScreen);
+	consoleInit(GFX_TOP, &topScreen);
 
-  consoleSelect(&topScreen);
+	consoleSelect(&topScreen);
 
-  printf("libctru streaming audio\n");
+	printf("libctru streaming audio\n");
 
-  u32 *audioBuffer = (u32*)linearAlloc(SAMPLESPERBUF*BYTESPERSAMPLE*2);
+	u32 *audioBuffer = (u32*)linearAlloc(SAMPLESPERBUF*BYTESPERSAMPLE*2);
 
-  bool fillBlock = false;
+	bool fillBlock = false;
 
-  ndspInit();
+	ndspInit();
 
-  ndspSetOutputMode(NDSP_OUTPUT_STEREO);
+	ndspSetOutputMode(NDSP_OUTPUT_STEREO);
 
-  ndspChnSetInterp(1, NDSP_INTERP_LINEAR);
-  ndspChnSetRate(1, SAMPLERATE);
-  ndspChnSetFormat(1, NDSP_FORMAT_STEREO_PCM16);
+	ndspChnSetInterp(0, NDSP_INTERP_LINEAR);
+	ndspChnSetRate(0, SAMPLERATE);
+	ndspChnSetFormat(0, NDSP_FORMAT_STEREO_PCM16);
 
-  float mix[12];
-  memset(mix, 0, sizeof(mix));
-  mix[0] = 1.0;
-  mix[1] = 1.0;
-  ndspChnSetMix(1, mix);
+	float mix[12];
+	memset(mix, 0, sizeof(mix));
+	mix[0] = 1.0;
+	mix[1] = 1.0;
+	ndspChnSetMix(0, mix);
 
-  int notefreq[] = {
-    262,
-    294,
-    339,
-    349,
-    392,
-    440,
-    494
-  };
+	int notefreq[] = {
+		262, 294, 339, 349, 392, 440,
+		494, 440, 392, 349, 339, 294
+	};
 
-  int note = 4;
+	int note = 4;
 
-  memset(waveBuf,0,sizeof(waveBuf));
-  waveBuf[0].data_vaddr = &audioBuffer[0];
-  waveBuf[0].nsamples = SAMPLESPERBUF;
-  waveBuf[1].data_vaddr = &audioBuffer[SAMPLESPERBUF];
-  waveBuf[1].nsamples = SAMPLESPERBUF;
+	memset(waveBuf,0,sizeof(waveBuf));
+	waveBuf[0].data_vaddr = &audioBuffer[0];
+	waveBuf[0].nsamples = SAMPLESPERBUF;
+	waveBuf[1].data_vaddr = &audioBuffer[SAMPLESPERBUF];
+	waveBuf[1].nsamples = SAMPLESPERBUF;
 
-  size_t stream_offset = 0;
+	size_t stream_offset = 0;
 
-  fill_buffer(audioBuffer,stream_offset, SAMPLESPERBUF * 2, notefreq[note]);
+	fill_buffer(audioBuffer,stream_offset, SAMPLESPERBUF * 2, notefreq[note]);
 
-  stream_offset += SAMPLESPERBUF;
+	stream_offset += SAMPLESPERBUF;
 
-  ndspChnWaveBufAdd(1, &waveBuf[0]);
-  ndspChnWaveBufAdd(1, &waveBuf[1]);
+	ndspChnWaveBufAdd(0, &waveBuf[0]);
+	ndspChnWaveBufAdd(0, &waveBuf[1]);
 
-  printf("Press up/down to change tone\n");
+	printf("Press up/down to change tone\n");
 
-  while(aptMainLoop()) {
+	while(aptMainLoop()) {
 
-    gfxSwapBuffers();
-    gfxFlushBuffers();
-    gspWaitForVBlank();
+		gfxSwapBuffers();
+		gfxFlushBuffers();
+		gspWaitForVBlank();
 
-    hidScanInput();
-    u32 kDown = hidKeysDown();
+		hidScanInput();
+		u32 kDown = hidKeysDown();
 
-    if (kDown & KEY_START)
-      break; // break in order to return to hbmenu
+		if (kDown & KEY_START)
+			break; // break in order to return to hbmenu
 
-    if (kDown & KEY_DOWN) {
-      note -= 1;
-      if (note<0) note = sizeof(notefreq)/sizeof(notefreq[0])-1;
-    }
+		if (kDown & KEY_DOWN) {
+			note -= 1;
 
-    if (kDown & KEY_UP) {
-      note += 1;
-      if (note > (sizeof(notefreq)/sizeof(notefreq[0])-1)) note = 0;
-    }
+			if (note<0) note = sizeof(notefreq)/sizeof(notefreq[0])-1;
+		}
 
-    if (waveBuf[fillBlock].status == NDSP_WBUF_DONE) {
+		if (kDown & KEY_UP) {
+			note += 1;
 
-      fill_buffer(waveBuf[fillBlock].data_pcm16, stream_offset, waveBuf[fillBlock].nsamples,notefreq[note]);
+			if (note > (sizeof(notefreq)/sizeof(notefreq[0])-1)) note = 0;
+		}
 
-      ndspChnWaveBufAdd(1, &waveBuf[fillBlock]);
-        stream_offset += waveBuf[fillBlock].nsamples;
+		if (waveBuf[fillBlock].status == NDSP_WBUF_DONE) {
 
-      fillBlock = !fillBlock;
-    }
-  }
+			fill_buffer(waveBuf[fillBlock].data_pcm16, stream_offset, waveBuf[fillBlock].nsamples,notefreq[note]);
 
-  ndspExit();
+			ndspChnWaveBufAdd(0, &waveBuf[fillBlock]);
+			stream_offset += waveBuf[fillBlock].nsamples;
 
-  linearFree(audioBuffer);
+			fillBlock = !fillBlock;
+		}
+	}
 
-  gfxExit();
-  return 0;
+	ndspExit();
+
+	linearFree(audioBuffer);
+
+	gfxExit();
+	return 0;
 }
