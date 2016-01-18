ffc26272b30a0d207c1a26ef5a962537f002d014
Dave Murphy <davem@devkitpro.org>
2016-01-18 03:39:05 +0000

    let's name the day and the month

--- time/rtc/source/main.c
+++ time/rtc/source/main.c
@@ -1,20 +1,45 @@
 /*
-	RTC example made by Aurelio Mannara for ctrulib
-	This code was modified for the last time on: 12/13/2014 2:45 UTC+1
-
-	This wouldn't be possible without the amazing work done by:
-	-Smealum
-	-fincs
-	-WinterMute
-	-yellows8
-	-plutoo
-	-mtheall
-	-Many others who worked on 3DS and I'm surely forgetting about
+	RTC example made by Aurelio Mannara for libctru
 */
 
 #include <3ds.h>
 #include <stdio.h>
 #include <time.h>
+
+
+const char* months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+const char* weekDays[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+
+const u16 daysAtStartOfMonthLUT[12] =
+{
+	0	%7, //januari		31
+	31	%7, //februari		28+1(leap year)
+	59	%7, //maart			31
+	90	%7, //april			30
+	120	%7, //mei			31
+	151	%7, //juni			30
+	181	%7, //juli			31
+	212	%7, //augustus		31
+	243	%7, //september		30
+	273	%7, //oktober		31
+	304	%7, //november		30
+	334	%7  //december		31
+};
+
+#define isLeapYear(year) (((year)%4) == 0)
+
+uint getDayOfWeek(uint day, uint month, uint year)
+{
+	//http://en.wikipedia.org/wiki/Calculating_the_day_of_the_week
+
+	day += 2*(3-((year/100)%4));
+	year %= 100;
+	day += year + (year/4);
+	day += daysAtStartOfMonthLUT[month] - (isLeapYear(year) && (month <= 1));
+	return day % 7;
+}
 
 int main(int argc, char **argv)
 {
@@ -49,7 +74,7 @@ int main(int argc, char **argv)
 		int year = timeStruct->tm_year +1900;
 
 		printf("\x1b[0;0H%02i:%02i:%02i", hours, minutes, seconds);
-		printf("\x1b[1;0H%02i/%02i/%04i", day, month, year);
+		printf("\n%s %s %i %i", weekDays[getDayOfWeek(day, month, year)], months[month], day, year);
 
 		// Flush and swap framebuffers
 		gfxFlushBuffers();
