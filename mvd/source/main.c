32dacbed469ed9a055512990c8147e9b60c39bb8
yellows8 <yellows8@users.noreply.github.com>
2016-04-20 15:08:31 -0400

    Updated the MVD example.

--- mvd/source/main.c
+++ mvd/source/main.c
@@ -3,24 +3,10 @@
 
 #include <3ds.h>
 
-#include "costable.h"
-
 u8* inaddr;
 u8* outaddr;
 
-char logstring[256];
-
-s32 pcCos(u16 v)
-{
-	return costable[v&0x1FF];
-}
-
-void printstring(char *str)//Placeholder until ctrulib itself has proper text drawing.
-{
-	strncat(logstring, str, sizeof(logstring)-1);
-}
-
-void draw_startup()
+void mvd_colorconvert()
 {
 	Result ret;
 
@@ -31,20 +17,7 @@ void draw_startup()
 
 	MVDSTD_Config config;
 
-	char str[256];
-
-	int i, j;
-	u32 cnt=0;
-	for(i=0;i<320;i++)
-	{
-		for(j=0;j<240;j++)
-		{
-			u32 v=(j+i*240)*3;
-			bufAdr[v]=(pcCos(i+cnt)+4096)/32;
-			bufAdr[v+1]=(pcCos(j-256+cnt)+4096)/64;
-			bufAdr[v+2]=(pcCos(i+128-cnt)+4096)/32;
-		}
-	}
+	printf("mvd color-format-conversion example.\n");
 
 	f = fopen("sdmc:/mvd_indata.bin", "r");
 	if(f)
@@ -59,39 +32,23 @@ void draw_startup()
 
 	memset(gfxtopadr, 0, 0x46500);
 	GSPGPU_FlushDataCache(inaddr, 0x46500);
-
-	printstring("mvd example\n");
+	GSPGPU_FlushDataCache(gfxtopadr, 0x46500);
 
 	ret = mvdstdInit(MVDMODE_COLORFORMATCONV, MVD_INPUT_YUYV422, MVD_OUTPUT_RGB565, 0);
-	memset(str, 0, 256);
-	snprintf(str, sizeof(str)-1, "mvdstdInit(): 0x%08x\n", (unsigned int)ret);
-	printstring(str);
+	printf("mvdstdInit(): 0x%08x\n", (unsigned int)ret);
 
 	if(ret>=0)
 	{
 		mvdstdGenerateDefaultConfig(&config, 320, 240, 320, 240, (u32*)inaddr, (u32*)outaddr, (u32*)&outaddr[0x12c00]);
 
 		ret = mvdstdProcessFrame(&config, NULL, 0, 0);
-		memset(str, 0, 256);
-		snprintf(str, sizeof(str)-1, "mvdstdProcessFrame(): 0x%08x\n", (unsigned int)ret);
-		printstring(str);
+		printf("mvdstdProcessFrame(): 0x%08x\n", (unsigned int)ret);
 	}
-
-	svcSleepThread(1000000000);//Not sure how to determine when frame processing finishes.
-
-	GSPGPU_InvalidateDataCache(outaddr, 0x100000);
 
 	f = fopen("sdmc:/mvd_outdata.bin", "w");
 	if(f)
 	{
 		fwrite(outaddr, 1, 0x100000, f);
-		fclose(f);
-	}
-
-	f = fopen("sdmc:/mvd_log", "w");
-	if(f)
-	{
-		fwrite(logstring, 1, strlen(logstring), f);
 		fclose(f);
 	}
 
@@ -106,20 +63,17 @@ void draw_startup()
 
 int main()
 {
-	gfxInitDefault();
-	//gfxSet3D(true); // uncomment if using stereoscopic 3D
+	int draw=1;
+	int ready=0;
+	int type=0;
 
-	memset(logstring, 0, 256);
+	gfxInitDefault();
+	consoleInit(GFX_BOTTOM, NULL);
 
 	inaddr = linearAlloc(0x100000);
 	outaddr = linearAlloc(0x100000);
 
-	if(inaddr && outaddr)
-	{
-		memset(inaddr, 0, 0x100000);
-		memset(outaddr, 0, 0x100000);
-		draw_startup();
-	}
+	if(inaddr && outaddr)ready=1;
 
 	// Main loop
 	while (aptMainLoop())
@@ -127,11 +81,57 @@ int main()
 		gspWaitForVBlank();
 		hidScanInput();
 
-		// Your code goes here
+		if(draw && type==0)
+		{
+			consoleClear();
+			draw = 0;
+
+			printf("mvd example\n");
+
+			if(!ready)
+			{
+				printf("Failed to allocate memory.\n");
+			}
+
+			printf("Press START to exit.\n");
+			if(ready)
+			{
+				printf("Press A for color-format-conversion.\n");
+			}
+		}
 
 		u32 kDown = hidKeysDown();
+
+		if(type)
+		{
+			if(kDown & KEY_A)
+			{
+				type = 0;
+				continue;
+			}
+		}
+
+		if(type)continue;
+
 		if (kDown & KEY_START)
 			break; // break in order to return to hbmenu
+
+		if(ready)
+		{
+			type = 0;
+			if (kDown & KEY_A)type = 1;
+
+			if(type)
+			{
+				memset(inaddr, 0, 0x100000);
+				memset(outaddr, 0, 0x100000);
+
+				if(type==1)mvd_colorconvert();
+
+				draw = 1;
+				printf("Press A to continue.\n");
+			}
+		}
 	}
 
 	if(inaddr)linearFree(inaddr);
