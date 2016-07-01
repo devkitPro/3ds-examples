ee1e46225f238b15b3bb3c9a2567c10b183df5bd
fincs <fincs.alt1@gmail.com>
2016-07-02 00:58:58 +0200

    Adapt to APT refactor - expand libapplet example

--- app_launch/source/main.c
+++ app_launch/source/main.c
@@ -2,17 +2,13 @@
 #include <string.h>
 #include <3ds.h>
 
-
-
 int main()
 {
 	gfxInitDefault(); // Init graphic stuff
 
-
-	// We need these 2 buffers for APT_DoAppJump() later. They can be smaller too
-	u8 buf0[0x300];
-	u8 buf1[0x20];
-
+	// We need these 2 buffers for APT_DoAppJump() later.
+	u8 param[0x300];
+	u8 hmac[0x20];
 
 	// Loop as long as the status is not exit
 	while(aptMainLoop())
@@ -23,17 +19,13 @@ int main()
 		if(hidKeysDown() & KEY_A) // If the A button got pressed, start the app launch
 		{
 			// Clear both buffers
-			memset(buf0, 0, 0x300);
-			memset(buf1, 0, 0x20);
+			memset(param, 0, sizeof(param));
+			memset(hmac, 0, sizeof(hmac));
 
-			// Open an APT session so we can talk to the APT service
-			aptOpenSession();
 			// Prepare for the app launch
-			APT_PrepareToDoAppJump(0, 0x0004001000022400LL, 0); // *EUR* camera app title ID
+			APT_PrepareToDoApplicationJump(0, 0x0004001000022400LL, 0); // *EUR* camera app title ID
 			// Tell APT to trigger the app launch and set the status of this app to exit
-			APT_DoAppJump(0x300 /* size of buf0 */, 0x20 /* size of buf1 */, buf0, buf1);
-			// Close the APT session because we don't need APT anymore
-			aptCloseSession();
+			APT_DoApplicationJump(param, sizeof(param), hmac);
 		}
 
 		// Flush + swap framebuffers and wait for VBlank. Not really needed in this example
@@ -41,7 +33,6 @@ int main()
 		gfxSwapBuffers();
 		gspWaitForVBlank();
 	}
-
 
 	gfxExit();
 
--- libapplet_launch/source/main.c
+++ libapplet_launch/source/main.c
@@ -1,5 +1,6 @@
 #include <3ds.h>
 #include <stdio.h>
+#include <string.h>
 
 static bool allowed = false;
 
@@ -16,7 +17,16 @@ int main()
 	consoleInit(GFX_TOP, NULL);
 	allowed = true;
 
-	printf("Press B to launch applet\n");
+	u32 aptbuf[0x400/4];
+
+	printf("Press A to launch extrapad\n");
+	printf("Press B to launch error\n");
+	printf("Press X to launch appleted (Mii Editor)\n");
+	printf("Press Y to launch memolib\n");
+	printf("Press L to launch Photo Selector\n");
+	printf("Press R to launch Sound Selector\n");
+	printf("Press ^ to launch mint (eShop)\n");
+	printf("Press < to launch software keyboard\n");
 
 	// Main loop
 	while (aptMainLoop())
@@ -31,15 +41,29 @@ int main()
 
 		gfxFlushBuffers();
 
-		// Launch the extrapad library applet when button B is pressed.
-		if (kDown & KEY_B)
+		if (kDown)
 		{
-			Result rc = APT_LaunchLibraryApplet(APPID_EXTRAPAD, 0, NULL, 0);
-			if (rc) printf("APT_LaunchLibraryApplet: %08lX\n", rc);
+			NS_APPID appId;
+
+			if (kDown & KEY_A) appId = APPID_EXTRAPAD;
+			else if (kDown & KEY_B) appId = APPID_ERROR;
+			else if (kDown & KEY_X) appId = APPID_APPLETED;
+			else if (kDown & KEY_Y) appId = APPID_MEMOLIB;
+			else if (kDown & KEY_L) appId = APPID_PNOTE_AP;
+			else if (kDown & KEY_R) appId = APPID_SNOTE_AP;
+			else if (kDown & KEY_UP) appId = APPID_MINT;
+			else if (kDown & KEY_LEFT) appId = APPID_SOFTWARE_KEYBOARD;
+			else continue;
+
+			memset(aptbuf, 0, sizeof(aptbuf));
+			if (!aptLaunchLibraryApplet(appId, aptbuf, sizeof(aptbuf), 0))
+				break;
+			printf("Library applet exited\n");
 		}
 	}
 
 	// Exit services
+	allowed = false;
 	gfxExit();
 	return 0;
 }
