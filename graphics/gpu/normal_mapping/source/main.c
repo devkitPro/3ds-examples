5707a84ea8210e8ea8177ec281f6de0abc0625e4
fincs <fincs.alt1@gmail.com>
2018-03-02 19:41:46 +0100

    Add graphics/gpu/normal_mapping example

---
+++ graphics/gpu/normal_mapping/source/main.c
@@ -0,0 +1,280 @@
+// Bump mapping (normal mapping) example
+// Texture borrowed from here:
+// http://polycount.com/discussion/89636/free-textures-to-use-i-only-ask-one-thing
+
+#include <3ds.h>
+#include <citro3d.h>
+#include <tex3ds.h>
+#include <string.h>
+#include "vshader_shbin.h"
+#include "diffuse_t3x.h"
+#include "normal_t3x.h"
+
+#define CLEAR_COLOR 0x68B0D8FF
+
+#define DISPLAY_TRANSFER_FLAGS \
+	(GX_TRANSFER_FLIP_VERT(0) | GX_TRANSFER_OUT_TILED(0) | GX_TRANSFER_RAW_COPY(0) | \
+	GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGB8) | \
+	GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO))
+
+typedef struct { float position[3]; float texcoord[2]; float normal[3]; float tangent[3]; } vertex;
+
+static const vertex vertex_list[] =
+{
+	// First face (PZ)
+	// First triangle
+	{ {-0.5f, -0.5f, +0.5f}, {0.0f, 0.0f}, {0.0f, 0.0f, +1.0f}, {+1.0f, 0.0f, 0.0f} },
+	{ {+0.5f, -0.5f, +0.5f}, {1.0f, 0.0f}, {0.0f, 0.0f, +1.0f}, {+1.0f, 0.0f, 0.0f} },
+	{ {+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, 0.0f, +1.0f}, {+1.0f, 0.0f, 0.0f} },
+	// Second triangle
+	{ {+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, 0.0f, +1.0f}, {+1.0f, 0.0f, 0.0f} },
+	{ {-0.5f, +0.5f, +0.5f}, {0.0f, 1.0f}, {0.0f, 0.0f, +1.0f}, {+1.0f, 0.0f, 0.0f} },
+	{ {-0.5f, -0.5f, +0.5f}, {0.0f, 0.0f}, {0.0f, 0.0f, +1.0f}, {+1.0f, 0.0f, 0.0f} },
+
+	// Second face (MZ)
+	// First triangle
+	{ {-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, +1.0f, 0.0f} },
+	{ {-0.5f, +0.5f, -0.5f}, {1.0f, 0.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, +1.0f, 0.0f} },
+	{ {+0.5f, +0.5f, -0.5f}, {1.0f, 1.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, +1.0f, 0.0f} },
+	// Second triangle
+	{ {+0.5f, +0.5f, -0.5f}, {1.0f, 1.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, +1.0f, 0.0f} },
+	{ {+0.5f, -0.5f, -0.5f}, {0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, +1.0f, 0.0f} },
+	{ {-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, +1.0f, 0.0f} },
+
+	// Third face (PX)
+	// First triangle
+	{ {+0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {+1.0f, 0.0f, 0.0f}, {0.0f, +1.0f, 0.0f} },
+	{ {+0.5f, +0.5f, -0.5f}, {1.0f, 0.0f}, {+1.0f, 0.0f, 0.0f}, {0.0f, +1.0f, 0.0f} },
+	{ {+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {+1.0f, 0.0f, 0.0f}, {0.0f, +1.0f, 0.0f} },
+	// Second triangle
+	{ {+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {+1.0f, 0.0f, 0.0f}, {0.0f, +1.0f, 0.0f} },
+	{ {+0.5f, -0.5f, +0.5f}, {0.0f, 1.0f}, {+1.0f, 0.0f, 0.0f}, {0.0f, +1.0f, 0.0f} },
+	{ {+0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {+1.0f, 0.0f, 0.0f}, {0.0f, +1.0f, 0.0f} },
+
+	// Fourth face (MX)
+	// First triangle
+	{ {-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	{ {-0.5f, -0.5f, +0.5f}, {1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	{ {-0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	// Second triangle
+	{ {-0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	{ {-0.5f, +0.5f, -0.5f}, {0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	{ {-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+
+	// Fifth face (PY)
+	// First triangle
+	{ {-0.5f, +0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, +1.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	{ {-0.5f, +0.5f, +0.5f}, {1.0f, 0.0f}, {0.0f, +1.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	{ {+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, +1.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	// Second triangle
+	{ {+0.5f, +0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, +1.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	{ {+0.5f, +0.5f, -0.5f}, {0.0f, 1.0f}, {0.0f, +1.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+	{ {-0.5f, +0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, +1.0f, 0.0f}, {0.0f, 0.0f, +1.0f} },
+
+	// Sixth face (MY)
+	// First triangle
+	{ {-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, -1.0f, 0.0f}, {+1.0f, 0.0f, 0.0f} },
+	{ {+0.5f, -0.5f, -0.5f}, {1.0f, 0.0f}, {0.0f, -1.0f, 0.0f}, {+1.0f, 0.0f, 0.0f} },
+	{ {+0.5f, -0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, -1.0f, 0.0f}, {+1.0f, 0.0f, 0.0f} },
+	// Second triangle
+	{ {+0.5f, -0.5f, +0.5f}, {1.0f, 1.0f}, {0.0f, -1.0f, 0.0f}, {+1.0f, 0.0f, 0.0f} },
+	{ {-0.5f, -0.5f, +0.5f}, {0.0f, 1.0f}, {0.0f, -1.0f, 0.0f}, {+1.0f, 0.0f, 0.0f} },
+	{ {-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}, {0.0f, -1.0f, 0.0f}, {+1.0f, 0.0f, 0.0f} },
+};
+
+#define vertex_list_count (sizeof(vertex_list)/sizeof(vertex_list[0]))
+
+static DVLB_s* vshader_dvlb;
+static shaderProgram_s program;
+static int uLoc_projection, uLoc_modelView;
+static C3D_Mtx projection;
+
+static C3D_LightEnv lightEnv;
+static C3D_Light light;
+static C3D_LightLut lut_Phong;
+
+static void* vbo_data;
+static C3D_Tex diffuse_tex, normal_tex;
+static float angleX = 0.0, angleY = 0.0;
+
+// Helper function for loading a texture from memory
+static bool loadTextureFromMem(C3D_Tex* tex, C3D_TexCube* cube, const void* data, size_t size)
+{
+	Tex3DS_Texture t3x = Tex3DS_TextureImport(data, size, tex, cube, false);
+	if (!t3x)
+		return false;
+
+	// Delete the t3x object since we don't need it
+	Tex3DS_TextureFree(t3x);
+	return true;
+}
+
+static void sceneInit(void)
+{
+	// Load the vertex shader, create a shader program and bind it
+	vshader_dvlb = DVLB_ParseFile((u32*)vshader_shbin, vshader_shbin_size);
+	shaderProgramInit(&program);
+	shaderProgramSetVsh(&program, &vshader_dvlb->DVLE[0]);
+	C3D_BindProgram(&program);
+
+	// Get the location of the uniforms
+	uLoc_projection = shaderInstanceGetUniformLocation(program.vertexShader, "projection");
+	uLoc_modelView  = shaderInstanceGetUniformLocation(program.vertexShader, "modelView");
+
+	// Configure attributes for use with the vertex shader
+	C3D_AttrInfo* attrInfo = C3D_GetAttrInfo();
+	AttrInfo_Init(attrInfo);
+	AttrInfo_AddLoader(attrInfo, 0, GPU_FLOAT, 3); // v0=position
+	AttrInfo_AddLoader(attrInfo, 1, GPU_FLOAT, 2); // v1=texcoord
+	AttrInfo_AddLoader(attrInfo, 2, GPU_FLOAT, 3); // v2=normal
+	AttrInfo_AddLoader(attrInfo, 3, GPU_FLOAT, 3); // v3=tangent
+
+	// Create the VBO (vertex buffer object)
+	vbo_data = linearAlloc(sizeof(vertex_list));
+	memcpy(vbo_data, vertex_list, sizeof(vertex_list));
+
+	// Configure buffers
+	C3D_BufInfo* bufInfo = C3D_GetBufInfo();
+	BufInfo_Init(bufInfo);
+	BufInfo_Add(bufInfo, vbo_data, sizeof(vertex), 4, 0x3210);
+
+	// Load the textures and bind them to their respective texture units
+	if (!loadTextureFromMem(&diffuse_tex, NULL, diffuse_t3x, diffuse_t3x_size))
+		svcBreak(USERBREAK_PANIC);
+	if (!loadTextureFromMem(&normal_tex, NULL, normal_t3x, normal_t3x_size))
+		svcBreak(USERBREAK_PANIC);
+	C3D_TexSetFilter(&diffuse_tex, GPU_LINEAR, GPU_NEAREST);
+	C3D_TexSetFilter(&normal_tex, GPU_LINEAR, GPU_NEAREST);
+	C3D_TexBind(0, &diffuse_tex);
+	C3D_TexBind(1, &normal_tex);
+
+	// Configure the texenv stages:
+	// 1) Combine primary fragment lighting color with texture color.
+	// 2) Add secondary fragment lighting color (specular component).
+	C3D_TexEnv* env = C3D_GetTexEnv(0);
+	C3D_TexEnvInit(env);
+	C3D_TexEnvSrc(env, C3D_Both, GPU_TEXTURE0, GPU_FRAGMENT_PRIMARY_COLOR, 0);
+	C3D_TexEnvFunc(env, C3D_RGB, GPU_MODULATE);
+	env = C3D_GetTexEnv(1);
+	C3D_TexEnvInit(env);
+	C3D_TexEnvSrc(env, C3D_Both, GPU_PREVIOUS, GPU_FRAGMENT_SECONDARY_COLOR, 0);
+	C3D_TexEnvFunc(env, C3D_RGB, GPU_ADD);
+
+	static const C3D_Material material =
+	{
+		{ 0.0f, 0.0f, 0.0f }, //ambient
+		{ 1.0f, 1.0f, 1.0f }, //diffuse
+		{ 0.4f, 0.4f, 0.4f }, //specular0
+		{ 0.0f, 0.0f, 0.0f }, //specular1
+		{ 0.0f, 0.0f, 0.0f }, //emission
+	};
+
+	C3D_LightEnvInit(&lightEnv);
+	C3D_LightEnvBind(&lightEnv);
+	C3D_LightEnvMaterial(&lightEnv, &material);
+	C3D_LightEnvBumpMode(&lightEnv, GPU_BUMP_AS_BUMP);
+	C3D_LightEnvBumpSel(&lightEnv, 1);
+
+	LightLut_Phong(&lut_Phong, 30);
+	C3D_LightEnvLut(&lightEnv, GPU_LUT_D0, GPU_LUTINPUT_LN, false, &lut_Phong);
+
+	C3D_FVec lightVec = FVec4_New(0.0f, 0.0f, -0.5f, 1.0f);
+
+	C3D_LightInit(&light, &lightEnv);
+	C3D_LightColor(&light, 1.0, 1.0, 1.0);
+	C3D_LightPosition(&light, &lightVec);
+}
+
+static void sceneRender(float iod)
+{
+	// Compute the projection matrix
+	Mtx_PerspStereoTilt(&projection, C3D_AngleFromDegrees(40.0f), C3D_AspectRatioTop, 0.01f, 1000.0f, iod, 2.0f, false);
+
+	// Calculate the modelView matrix
+	C3D_Mtx modelView;
+	Mtx_Identity(&modelView);
+	Mtx_Translate(&modelView, 0.0, 0.0, -3.0 /*+ sinf(angleX)*/, true);
+	Mtx_RotateX(&modelView, angleX, true);
+	Mtx_RotateY(&modelView, angleY, true);
+	Mtx_Scale(&modelView, 1.5f, 1.5f, 1.5f);
+
+	// Update the uniforms
+	C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_projection, &projection);
+	C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_modelView,  &modelView);
+
+	// Draw the VBO
+	C3D_DrawArrays(GPU_TRIANGLES, 0, vertex_list_count);
+}
+
+static void sceneExit(void)
+{
+	// Free the textures
+	C3D_TexDelete(&diffuse_tex);
+	C3D_TexDelete(&normal_tex);
+
+	// Free the VBO
+	linearFree(vbo_data);
+
+	// Free the shader program
+	shaderProgramFree(&program);
+	DVLB_Free(vshader_dvlb);
+}
+
+int main()
+{
+	// Initialize graphics
+	gfxInitDefault();
+	gfxSet3D(true); // Enable stereoscopic 3D
+	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+
+	// Initialize the render targets
+	C3D_RenderTarget* targetLeft  = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
+	C3D_RenderTarget* targetRight = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
+	C3D_RenderTargetSetOutput(targetLeft,  GFX_TOP, GFX_LEFT,  DISPLAY_TRANSFER_FLAGS);
+	C3D_RenderTargetSetOutput(targetRight, GFX_TOP, GFX_RIGHT, DISPLAY_TRANSFER_FLAGS);
+
+	// Initialize the scene
+	sceneInit();
+
+	// Main loop
+	while (aptMainLoop())
+	{
+		hidScanInput();
+
+		// Respond to user input
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_START)
+			break; // break in order to return to hbmenu
+
+		float slider = osGet3DSliderState();
+		float iod = slider/3;
+
+		// Rotate the cube each frame
+		angleX += C3D_AngleFromDegrees(1.0);
+		angleY += C3D_AngleFromDegrees(0.5);
+
+		// Render the scene
+		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+		{
+			C3D_RenderTargetClear(targetLeft, C3D_CLEAR_ALL, CLEAR_COLOR, 0);
+			C3D_FrameDrawOn(targetLeft);
+			sceneRender(-iod);
+
+			if (iod > 0.0f)
+			{
+				C3D_RenderTargetClear(targetRight, C3D_CLEAR_ALL, CLEAR_COLOR, 0);
+				C3D_FrameDrawOn(targetRight);
+				sceneRender(iod);
+			}
+		}
+		C3D_FrameEnd(0);
+	}
+
+	// Deinitialize the scene
+	sceneExit();
+
+	// Deinitialize graphics
+	C3D_Fini();
+	gfxExit();
+	return 0;
+}
